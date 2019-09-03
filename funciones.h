fa3ef87fc7079f3fcf65bb1a47ea060e7e8083eb
gastonlf <54329469+gastonlf@users.noreply.github.com>
2019-09-03 11:37:30 -0300

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,56 @@
+int sumar   (int operandoUno,int operandoDos); /**
+* \brief Recibe 2 numeros
+* \param carga laprimer variable
+* \param carga la segunda variable
+* \param Se ejecuta la suma entre las variables
+* \return La suma de ambos numeros
+*
+*/
+
+int restar  (int operandoUno, int operandoDos);
+/**
+* \brief Recibe 2 numeros
+* \param carga laprimer variable
+* \param carga la segunda variable
+* \param Se ejecuta la resta entre las variables
+* \return La resta de ambos numeros
+*
+*/
+int multiplicar (int operandoUno, int operandoDos);
+/**
+* \brief Recibe 2 numeros
+* \param carga la primer variable
+* \param carga la segunda variable
+* \param Se ejecuta la multiplicacion entre las variables
+* \return La multiplicacion de ambos numeros
+*
+*/
+
+float dividir (float operandoUno, float operandoDos);
+/**
+* \brief Recibe 2 numeros
+* \param carga laprimer variable
+* \param carga la segunda variable
+* \param Se ejecuta la division entre las variables
+* \return La division de ambos numeros
+*
+*/
+
+int factorizar (int operandoUno);
+/**
+* \brief Recibe 1 numero
+* \param carga la variable
+* \param inicia un contador segun el nº ingresado
+* \param Se ejecuta la factorizacion entre las variables
+* \return La variable correspondiente
+*
+*/
+int factorizar (int operadorDos);
+/**
+* \brief Recibe 1 numero
+* \param carga la variable
+* \param inicia un contador segun el nº ingresado
+* \param Se ejecuta la factorizacion entre las variables
+* \return La variable correspondiente
+*
+*/
