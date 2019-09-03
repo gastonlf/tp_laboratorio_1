434acab064b4d156c97251639164276800ec3351
gastonlf <54329469+gastonlf@users.noreply.github.com>
2019-09-03 12:17:42 -0300

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,84 @@
+
+
+/**
+* \brief Recibe 2 numeros
+* \param carga laprimer variable
+* \param carga la segunda variable
+* \param Se ejecuta la suma entre las variables
+* \return La suma de ambos numeros
+*
+*/
+int sumar (int operandoUno,int operandoDos) //operacion para sumar
+{
+        int resultado;
+        resultado = operandoUno + operandoDos;
+        return resultado;
+}
+
+
+/**
+* \brief Recibe 2 numeros
+* \param carga laprimer variable
+* \param carga la segunda variable
+* \param Se ejecuta la resta entre las variables
+* \return La resta de ambos numeros
+*
+*/
+
+int restar(int operandoUno,int operandoDos)
+{
+        int resultado;
+        resultado = operandoUno - operandoDos;
+        return resultado;
+}
+
+/**
+* \brief Recibe 2 numeros
+* \param carga la primer variable
+* \param carga la segunda variable
+* \param Se ejecuta la multiplicacion entre las variables
+* \return La multiplicacion de ambos numeros
+*
+*/
+int multiplicar(int operandoUno,int operandoDos)
+{
+    int resultado;
+    resultado = operandoUno * operandoDos;
+    return resultado;
+}
+
+/**
+* \brief Recibe 2 numeros
+* \param carga laprimer variable
+* \param carga la segunda variable
+* \param Se ejecuta la division entre las variables
+* \return La division de ambos numeros
+*
+*/
+float dividir (float operandoUno, float operandoDos)
+{
+    int resultado;
+    resultado = operandoUno / operandoDos;
+    return resultado;
+
+}
+/**
+* \brief Recibe 1 numero
+* \param carga la variable
+* \param inicia un contador segun el nº ingresado
+* \param Se ejecuta la factorizacion entre las variables
+* \return La variable correspondiente
+*
+*/
+
+    int factorizar(int operadorUno)
+{
+    int i,factorial=1;
+
+    for (i=1;i<=operadorUno;i++)
+    {
+      factorial*=i;
+    }
+    return factorial;
+
+}
---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+int main()
+{
+    int menu;
+    float op1=0,op2=0;
+    float suma,resta,multiplicacion,factorial1,factorial2;
+    float division;
+
+
+
+    do{
+
+    printf("\n1- INGRESAR PRIMER NUMERO \n2- INGRESAR SEGUNDO NUMERO \n3- CALCULAR TODAS LAS OPERACIONES \n4- MOSTRAR TODOS TODOS LOS RESULTADOS \n5- CERRAR PROGRAMA\n");
+    printf("Opcion: ");
+    scanf("%i",&menu);
+    system("cls");
+
+
+    switch(menu)
+{
+    case 1: printf("Ingrese OP1: ");
+            fflush(stdin);
+            scanf("%f",&op1);
+            break;
+    case 2: printf("Ingrese OP2: ");
+            fflush(stdin);
+            scanf("%f",&op2);
+            break;
+    case 3:
+            suma = sumar(op1,op2);
+
+            resta = restar(op1,op2);
+
+            multiplicacion = multiplicar(op1,op2);
+
+            division = dividir(op1,op2);
+
+            factorial1   =   factorizar(op1);
+            factorial2  =    factorizar(op2);
+
+            printf("\n\n***SE REALIZARON TODAS LAS OPERACIONES.***\n\n");
+
+            break;
+    case 4:         printf("\n\nLa suma es:%.0f ",suma);
+
+                    printf("\n\nLa resta es:%.0f ",resta);
+
+                    printf("\n\nLa multiplicacion es:%.0f ",multiplicacion);
+
+                    printf("\n\nLas factoriales son: %.0f y %.0f ",factorial1,factorial2);
+
+                    if (op2==0){
+                    printf("\n\nNo se puede dividir por cero.");
+                    }
+                    else {
+                    printf("\n\nLa division es :%.2f \n",division);
+                    }
+
+
+}
+}while(menu!=5);
+
+
+}//main
